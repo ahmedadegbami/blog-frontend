9d17701b7c1010ce384e46c8e9a67ce900bdb7b3
ahmedadegbami <ahmed.adegbami@gmail.com>
2022-05-17 23:29:48 +0200

    Update BlogList.jsx

--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import posts from "../../../data/posts.json";
 import BlogItem from "../blog-item/BlogItem";
 
-const BlogList = (props) => {
+const BlogList = () => {
+  const [authors, setAuthors] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const apiUrl = process.env.REACT_APP_API_URL;
+    const response = await fetch(`${apiUrl}/authors`);
+
+    const data = await response.json();
+    setAuthors(data);
+    console.log(data);
+  };
+
   return (
     <Row>
-      {posts.map((post) => (
+      {authors.map((author) => (
         <Col
           md={4}
           style={{
             marginBottom: 50,
           }}
         >
-          <BlogItem key={post.title} {...post} />
+          <BlogItem author={author} />
         </Col>
       ))}
     </Row>
@@ -21,3 +36,27 @@ const BlogList = (props) => {
 };
 
 export default BlogList;
+
+// import React from "react";
+// import { Col, Row } from "react-bootstrap";
+// import posts from "../../../data/posts.json";
+// import BlogItem from "../blog-item/BlogItem";
+
+// const BlogList = (props) => {
+//   return (
+//     <Row>
+//       {posts.map((post) => (
+//         <Col
+//           md={4}
+//           style={{
+//             marginBottom: 50,
+//           }}
+//         >
+//           <BlogItem key={post.title} {...post} />
+//         </Col>
+//       ))}
+//     </Row>
+//   );
+// };
+
+// export default BlogList;
